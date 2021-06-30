eb3b5f64bb23c33d7355b9c24e9956579d131bf4
Sergio Torres <sdtorresl@unal.edu.co>
2021-06-29 23:41:48 -0500

    Fixes in journey

--- src/app/components/journey/journey.component.ts
+++ src/app/components/journey/journey.component.ts
@@ -1,25 +1,43 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-journey',
   templateUrl: './journey.component.html',
   styleUrls: ['./journey.component.scss']
 })
-export class JourneyComponent implements OnInit {
+export class JourneyComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('widgetsContent') widgetsContent!: ElementRef;
+  leftActive: boolean = true;
+  rightActive: boolean = true;
 
   constructor() { }
 
+  ngAfterViewInit(): void {
+    this.checkActiveArrows();
+  }
+
   ngOnInit(): void {
-
   }
 
-  @ViewChild('widgetsContent') widgetsContent!: ElementRef;
-
-  scrollLeft(){
-    this.widgetsContent.nativeElement.scrollLeft -= 150;
+  scrollLeft() {
+    const widgetContent = this.widgetsContent.nativeElement;
+    widgetContent.scrollLeft -= 150;
+    this.checkActiveArrows();
   }
 
-  scrollRight(){
-    this.widgetsContent.nativeElement.scrollLeft += 150;
+  scrollRight() {
+    const widgetContent = this.widgetsContent.nativeElement;
+    widgetContent.scrollLeft += 150;
+    this.checkActiveArrows();
+  }
+
+  private checkActiveArrows() {
+    const widgetContent = this.widgetsContent.nativeElement;
+    const maxScroll = widgetContent.scrollWidth - widgetContent.clientWidth;
+    const minScroll = 0;
+
+    this.leftActive = !(widgetContent.scrollLeft <= minScroll);
+    this.rightActive = !(widgetContent.scrollLeft >= maxScroll);
   }
 }
